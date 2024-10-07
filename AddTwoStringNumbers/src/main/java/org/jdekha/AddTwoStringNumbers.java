c254525027f3597c50c19987dda7764510b8f5b2
abdelhak <jdekha75@gmail.com>
2024-10-07 16:29:59 +0200

    v2

--- AddTwoStringNumbers/src/main/java/org/jdekha/AddTwoStringNumbers.java
+++ AddTwoStringNumbers/src/main/java/org/jdekha/AddTwoStringNumbers.java
@@ -1,5 +1,8 @@
 package org.jdekha;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class AddTwoStringNumbers {
@@ -9,14 +12,43 @@ public class AddTwoStringNumbers {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
 
-        String z = "1".repeat(5);
-        System.out.println(z);
-        System.out.println(addStrNums("999", "1001"));
-        System.out.println(addStrNums("12300", "300021"));
-        System.out.println(addStrNums("189", "921"));
+//        String z = "1".repeat(5);
+//        System.out.println(z);
+//        System.out.println(addStrNums("999", "1001"));
+//        System.out.println(addStrNums("12300", "300021"));
+        System.out.println(addStrNums("13300", "3367"));
     }
 
     public static String addStrNums(String n1, String n2) {
+        int padding = Math.abs(n1.length() - n2.length());
+        if (padding != 0) {
+            String zeroPad = "0".repeat(padding);
+            if (n1.length() < n2.length()) {
+                n1 = zeroPad + n1;
+            } else {
+                n2 = zeroPad + n2;
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        char[] tab1 = new StringBuilder(n1).reverse().toString().toCharArray();
+        char[] tab2 = new StringBuilder(n2).reverse().toString().toCharArray();
+        int retenue = 0;
+        for (int i = 0; i < tab1.length; i++) {
+            int somme = ((int) tab1[i] + (int) tab2[i] + retenue) % 48;
+            int unite = somme % 10;
+            retenue = somme / 10;
+            //System.out.println(tab2[i]);
+            System.out.println(unite);
+            stringBuilder.append(unite);
+            //System.out.println(somme);
+        }
+        if (retenue == 1) {
+            stringBuilder.append(retenue);
+        }
+        return stringBuilder.reverse().toString();
+    }
+
+    public static String addStrNums1(String n1, String n2) {
 
         int padding = Math.abs(n1.length() - n2.length());
         if (padding != 0) {
